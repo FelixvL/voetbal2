3be3005a2b2459b3d74c352eb9fc5726e4e875a4
FelixvL <digitaleservice@gmail.com>
2020-02-20 16:09:05 +0100

    go

--- Demo.java
+++ Demo.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 class Demo{
    public static void main(String[] args) {
 	   Team a = new Team();
+	   Team b = new Team();
 	   a.introduceren();
+	   b.introduceren();
    }
 }
 class Team{
@@ -13,10 +15,14 @@ class Team{
 	Speler aanvaller;
 	Team( ){
 		this.vraagTeamNaam();
-		this.keeper = new Speler();
+		this.keeper = new Keeper();
+		this.aanvaller = new Aanvaller();
+		this.verdediger = new Verdediger();
 	}
 	void introduceren() {
 		System.out.println(this.naam + " en mijn \nkeeper heet: "+keeper.naam);
+		System.out.println(" en mijn \naanvaller heet: "+aanvaller.naam);
+		System.out.println(" en mijn \nverdediger heet: "+verdediger.naam);
 	}
 	void keeperNaamGeven(String naam) {
 		this.keeper.naam = naam;
@@ -27,9 +33,16 @@ class Team{
 		naam = sc.nextLine();
 	}
 }
-class Speler{
+abstract class Speler{
 	String naam;
 	Speler(){
+		
+	}
+	
+
+}
+class Keeper extends Speler{
+	Keeper(){
 		this.vraagSpelerNaam();
 	}
 	void vraagSpelerNaam() {
@@ -38,3 +51,40 @@ class Speler{
 		naam = sc.nextLine();
 	}
 }
+class Aanvaller extends Speler{
+	Aanvaller(){
+		this.vraagSpelerNaam();
+	}
+	void vraagSpelerNaam() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Aanvaller?");
+		naam = sc.nextLine();
+	}	
+}
+class Verdediger extends Speler{
+	Verdediger(){
+		this.vraagSpelerNaam();
+	}
+	void vraagSpelerNaam() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Verdediger?");
+		naam = sc.nextLine();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
