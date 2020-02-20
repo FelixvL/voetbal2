0d0b0a6c8bacc4e90bc2c4205252c34ed70e72ec
kf03w5t5741l <36108518+kf03w5t5741l@users.noreply.github.com>
2020-02-20 15:59:00 +0100

    Rollen toevoegen

--- Demo.java
+++ Demo.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 class Demo{
    public static void main(String[] args) {
 	   Team a = new Team();
+	   Team b = new Team();
+
 	   a.introduceren();
+	   b.introduceren();
    }
 }
 class Team{
@@ -11,30 +14,37 @@ class Team{
 	Speler keeper;
 	Speler verdediger;
 	Speler aanvaller;
+
 	Team( ){
 		this.vraagTeamNaam();
-		this.keeper = new Speler();
+		this.keeper = new Speler("keeper");
+		this.verdediger = new Speler("verdediger");
+		this.aanvaller = new Speler("aanvaller");
 	}
+
 	void introduceren() {
 		System.out.println(this.naam + " en mijn \nkeeper heet: "+keeper.naam);
 	}
-	void keeperNaamGeven(String naam) {
-		this.keeper.naam = naam;
-	}
+
 	void vraagTeamNaam() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Team?");
 		naam = sc.nextLine();
 	}
 }
-class Speler{
+class Speler {
 	String naam;
-	Speler(){
+	String rol;
+
+	Speler(String rol){
+		this.rol = rol;
 		this.vraagSpelerNaam();
 	}
+
 	void vraagSpelerNaam() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Keeper?");
+		System.out.println("Naam (" + this.rol + "): ");
 		naam = sc.nextLine();
 	}
+
 }
