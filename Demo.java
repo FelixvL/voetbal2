dc69b70b6cdbdcabf49cbf9c40aed3216fede7a5
FelixvL <digitaleservice@gmail.com>
2020-02-20 13:55:18 +0100

    go

---
+++ Demo.java
@@ -0,0 +1,17 @@
+package voetbal2;
+
+class Demo{
+   public static void main(String[] args) {
+	   Team team1 = new Team();
+	   Team team2 = new Team();
+   }
+}
+class Team{
+	Speler keeper = new Speler();
+	Speler verdediger = new Speler();
+	Speler aanvaller = new Speler();
+	
+}
+class Speler{
+	
+}
