67672c139031054d89f3cb657af87b5e280148af
FelixvL <digitaleservice@gmail.com>
2020-02-20 14:13:04 +0100

    jo

--- Demo.java
+++ Demo.java
@@ -2,17 +2,30 @@ package voetbal2;
 
 class Demo{
    public static void main(String[] args) {
-	   Team team1 = new Team();
-	   Team team2 = new Team();
+	   Team team1 = new Team("Ajax");
+	   Team team2 = new Team("Feyenoord");
 	   System.out.println("hoi");
+	   team1.keeperNaamGeven("Menzo");
+	   
+	   team1.introduceren();
    }
 }
 class Team{
+	String naam = "ajax";
 	Speler keeper = new Speler();
 	Speler verdediger = new Speler();
 	Speler aanvaller = new Speler();
-	
+	Team( String a ){
+		this.naam = a;
+	}
+	void introduceren() {
+		System.out.println(this.naam + " en mijn \nkeeper heet: "+keeper.naam);
+	}
+	void keeperNaamGeven(String naam) {
+		this.keeper.naam = naam;
+	}
 }
 class Speler{
+	String naam = "onana";
 	
 }
